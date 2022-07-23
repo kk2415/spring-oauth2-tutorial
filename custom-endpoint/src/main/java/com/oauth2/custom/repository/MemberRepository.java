d18c48b6791ea97fa089e22aabc74b8b5cdc09da
Kim kyunghee <kkh2415@naver.com>
2022-07-23 23:07:46 +0900

    add : #5-Member 엔티티와 연관된 클래스 생성

---
+++ custom-endpoint/src/main/java/com/oauth2/custom/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.oauth2.custom.repository;
+
+import com.oauth2.custom.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+}
